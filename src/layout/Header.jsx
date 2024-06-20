795f218e1ffde8179dd8cb5683adda7503050390
Ilya Petrov <ilya.petrov.m@gmail.com>
2024-06-20 22:42:47 +0300

    Minimal structure

---
+++ src/layout/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header() {
+    return (
+        <nav className="cyan darken-1">
+            <div className="nav-wrapper">
+                <a href="#" className="brand-logo">Реактивные фильмы</a>
+                <ul id="nav-mobile" className="right hide-on-med-and-down">
+                    <li><a href="!#">Repo</a></li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
